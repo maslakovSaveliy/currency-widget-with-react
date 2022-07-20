da54f6ef76f0e6059ee67410eaad90412cc91905
sssaaaaaammm <saveliymaslakov969@gmail.com>
2022-07-20 14:14:13 +0300

    fix

--- src/components/Convertor.jsx
+++ src/components/Convertor.jsx
@@ -4,6 +4,7 @@ import axios from "axios";
 const Convertor = () => {
   const [currency, setCurrency] = useState({
     name: "",
+    nominal: "",
     price: "",
   });
   const fetchCurrency = async () => {
@@ -12,6 +13,7 @@ const Convertor = () => {
     );
     setCurrency({
       name: response.data.Valute.USD.CharCode,
+      nominal: response.data.Valute.USD.Nominal,
       price: response.data.Valute.USD.Value,
     });
   };
@@ -23,9 +25,16 @@ const Convertor = () => {
       <div className="content">
         <h1>{currency.name}</h1>
         <span className="currency">
-          <h2>1 {currency.name}</h2>
+          <h2>
+            {currency.nominal} {currency.name}
+          </h2>
           <h2>=</h2>
-          <h2>{Math.floor(currency.price * 100) / 100} RUB</h2>
+          <h2>
+            {currency.price.toLocaleString("ru", {
+              style: "currency",
+              currency: "RUB",
+            })}
+          </h2>
         </span>
       </div>
     </div>
